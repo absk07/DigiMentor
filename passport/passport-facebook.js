bcb21879c913143d2406e52e8bb0f2a84d30b403
Abhishek <89835803+absk07@users.noreply.github.com>
2024-07-10 13:42:24 +0530

    Update passport-facebook.js

--- passport/passport-facebook.js
+++ passport/passport-facebook.js
@@ -11,8 +11,8 @@ passport.deserializeUser(function(user, done) {
 });
 
 passport.use(new facebookStrategy({
-    clientID: '461493178385306',
-    clientSecret: 'ecce83d841acba3030b2df49632cba12',
+    clientID: 'CLIENT_ID',
+    clientSecret: 'CLIENT_SECRET',
     profileFields: ['displayName', 'photos', 'email'],
     callbackURL: 'http://localhost:3000/auth/facebook/callback',
     passReqToCallback: true
